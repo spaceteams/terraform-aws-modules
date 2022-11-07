14f812f272b56c069394fe7485d91635d11e74e9
Christian Kampka <christian.kampka@spaceteams.de>
2022-11-07 17:53:27 +0100

    Add pre-commit hook config

--- flake.nix
+++ flake.nix
@@ -3,15 +3,29 @@
 
   inputs.utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShell = pkgs.mkShell {
-          name = "terraform-aws-modules";
-          packages = with pkgs; [ terraform jq tflint terraform-ls go gnumake ];
-        };
-      });
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShell = pkgs.mkShell {
+        name = "terraform-aws-modules";
+        packages = with pkgs; [
+          terraform
+          jq
+          tflint
+          terraform-ls
+          pre-commit
+          go
+          gnumake
+        ];
+
+        shellHook = ''
+          [ -e "$(git rev-parse --git-dir)/hooks/pre-commit" ] || ${pkgs.pre-commit}/bin/pre-commit install
+        '';
+      };
+    });
 }
