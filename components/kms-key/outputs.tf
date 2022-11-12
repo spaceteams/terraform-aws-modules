7a6a14309b12de4a8d5e45eaea1cc6138d3cb8f8
Christian Kampka <christian.kampka@spaceteams.de>
2022-11-12 11:50:31 +0100

    Refactor deprecated legacy splat expressions
    See
    https://developer.hashicorp.com/terraform/language/expressions/splat#legacy-attribute-only-splat-expressions
    for details

--- components/kms-key/outputs.tf
+++ components/kms-key/outputs.tf
@@ -1,21 +1,21 @@
 output "key_arn" {
-  value = join("", aws_kms_key.this.*.arn)
+  value = join("", [for key in aws_kms_key.this : key.arn])
 
   description = "The ARN of the KMS key"
 }
 
 output "key_id" {
-  value = join("", aws_kms_key.this.*.id)
+  value = join("", [for key in aws_kms_key.this : key.id])
 
   description = "The ID of the KMS key"
 }
 output "alias_name" {
-  value = join("", aws_kms_alias.this.*.name)
+  value = join("", [for alias in aws_kms_alias.this : alias.name])
 
   description = "The alias name associated with the KMS key"
 }
 output "alias_arn" {
-  value = join("", aws_kms_alias.this.*.arn)
+  value = join("", [for alias in aws_kms_alias.this : alias.arn])
 
   description = "The ARN of the alias associated with the KMS key"
 }
